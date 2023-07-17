8f9125c2bbc2478289d877dcea099114d3946222
rajankit00007 <107411695+rajankit00007@users.noreply.github.com>
2023-07-17 22:26:44 +0530

    Update two pointer sort .cpp
    sort 0 to 1

--- two pointer sort.cpp
+++ two pointer sort.cpp
@@ -1,43 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// void sortZerosandOnes(vector<int> &v){
-//     int zeros_count = 0;
-//     for(int ele:v){
-//         if(ele == 0){
-//         zeros_count++;
-//         }
-//     }
-//     for(int i=0; i<v.size();i++){
-//         if(i<zeros_count){
-//             v[i]=0;
-//         }else{
-//             v[i]=1;
-//         }
+void sortZerosandOnes(vector<int> &v){
+    int zeros_count = 0;
+    for(int ele:v){
+        if(ele == 0){
+        zeros_count++;
+        }
+    }
+    for(int i=0; i<v.size();i++){
+        if(i<zeros_count){
+            v[i]=0;
+        }else{
+            v[i]=1;
+        }
 
-//     }
+    }
 
-// }
+}
 
-// int main(){
-//     int n;
-//     cin>>n;
+int main(){
+    int n;
+    cin>>n;
 
-//     vector<int>v;
+    vector<int>v;
 
-//     for(int i=0;i<n;i++)
-//     {
-//         int ele;
-//         cin>>ele;
-//         v.push_back(ele);
-//     }
+    for(int i=0;i<n;i++)
+    {
+        int ele;
+        cin>>ele;
+        v.push_back(ele);
+    }
     
-//     sortZerosandOnes(v);
-//     for(int i=0;i<n;i++){
-//         cout<<v[i]<<" ";
-//     }
-//     cout<<endl;
+    sortZerosandOnes(v);
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
 
+//without using function
+#include<bits/stdc++.h>
+using namespace std;
 int main(){
     int n;
     cin>>n;
