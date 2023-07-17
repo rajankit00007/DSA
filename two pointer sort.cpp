8556d305578bac619df18c1c187ea1989cd5029a
rajankit00007 <107411695+rajankit00007@users.noreply.github.com>
2023-07-17 22:24:14 +0530

    Add files via upload

---
+++ two pointer sort.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void sortZerosandOnes(vector<int> &v){
+//     int zeros_count = 0;
+//     for(int ele:v){
+//         if(ele == 0){
+//         zeros_count++;
+//         }
+//     }
+//     for(int i=0; i<v.size();i++){
+//         if(i<zeros_count){
+//             v[i]=0;
+//         }else{
+//             v[i]=1;
+//         }
+
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     vector<int>v;
+
+//     for(int i=0;i<n;i++)
+//     {
+//         int ele;
+//         cin>>ele;
+//         v.push_back(ele);
+//     }
+    
+//     sortZerosandOnes(v);
+//     for(int i=0;i<n;i++){
+//         cout<<v[i]<<" ";
+//     }
+//     cout<<endl;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++){
+    int ele;
+    cin>>ele;
+    v.push_back(ele);
+    }
+
+    int count_zeros =0;
+    for(int i=0;i<v.size();i++){
+        if(v[i]==0){
+            count_zeros++;
+        }
+    }
+    for(int i=0;i<v.size();i++){
+        if(i<count_zeros){
+            v[i]=0;
+        }else{
+            v[i]=1;
+        }
+        cout<<v[i];
+    }
+}
