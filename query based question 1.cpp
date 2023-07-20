12fb9f9c7110ab8693600bc034737b44ed2658c3
rajankit00007 <107411695+rajankit00007@users.noreply.github.com>
2023-07-20 23:07:14 +0530

    Add files via upload

---
+++ query based question 1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v(n+1,0);
+    for(int i=1;i<=n;i++){
+        cin>>v[i];
+    }
+
+    //calculate the prefix sum
+    for(int i=1;i<=n;i++){
+        v[i]+=v[i-1];
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        int ans=0;
+
+        ans=v[r]-v[l-1];
+        cout<<ans<<endl;
+    }
+}
