cf113f5df1ef30ac8533f05325646bac28efa0de
rajankit00007 <107411695+rajankit00007@users.noreply.github.com>
2023-08-07 22:41:20 +0530

    Update leetcode three sum.cpp

--- leetcode three sum.cpp
+++ leetcode three sum.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
+        
+         int target = 0;
         sort(nums.begin(), nums.end());
-        set<vector<int>> set;
+        set<vector<int>> s;
         vector<vector<int>> output;
-        for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
-                for(int k=j+1; k<n; k++){
-                    if((nums[i] + nums[j] + nums[k] == 0) && i != j && j != k && k != i){
-                        set.insert({nums[i], nums[j], nums[k]});
-                    }
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-        for(auto it : set){
-            output.push_back(it);
-        }
+        for(auto triplets : s)
+            output.push_back(triplets);
         return output;
+    
     }
 };
