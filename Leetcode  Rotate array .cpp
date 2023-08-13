048dfb9e9a063e417b4f12e17c01921e008fc4ed
rajankit00007 <107411695+rajankit00007@users.noreply.github.com>
2023-08-13 21:25:24 +0530

    Create Leetcode  Rotate array .cpp

---
+++ Leetcode  Rotate array .cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverse(vector<int> &num,int low,int high){
+         while(low<high){
+          swap(num[low],num[high]);
+          low++;
+          high--;
+      }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+     reverse(nums,0,n-k-1);
+     reverse(nums,n-k,n-1);
+     reverse(nums,0,n-1);
+    }
+};
