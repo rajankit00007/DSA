c6144f342335037044f419cea3a2de5e4702f3c9
rajankit00007 <107411695+rajankit00007@users.noreply.github.com>
2023-09-20 13:37:42 +0530

    Create Leetcode SQRT by binarysearch .cpp

---
+++ Leetcode SQRT.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int binarySearch(int n){
+        int s=0;
+        int e = n;
+        int mid = s+(e-s)/2;
+        int ans = -1;
+        while(s<=e){
+            int square  = mid*mid;
+            if(square == n){
+                return mid;
+            }
+            if(square < n){
+                ans = mid;
+                s= mid+1; 
+            }else{
+                e = mid-1;
+            }
+            mid = s+(e-s)/2;
+        }
+        return ans;
+    }
+    int mySqrt(int x) {
+      return  binarySearch(x);
+    }
+};
